77ee12ed8bf55e3013a00b344bc0688e87671e2b
abigailmgarcia <abigailmg31@gmail.com>
2023-08-01 17:51:51 -0500

    styled first page

--- abigails-portfolio/src/components/intro/Intro.jsx
+++ abigails-portfolio/src/components/intro/Intro.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
 import "./intro.scss"
-
+import {AiOutlineArrowDown} from "react-icons/ai"
 export default function intro() {
   return (
-    <div className='intro' id="intro"></div>
+    <div className='intro' id="intro">
+      <div className="left">
+        <div className="imgContainer">
+          <img src="/assets/img_6226.jpg" alt="" />
+        </div>
+      </div>
+  
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hello Everyone, I am</h2>
+          <h1>Abigail Garcia</h1>
+          <h3>A Full-Stack Web Developer <span></span></h3>
+        </div>
+        <a href="#portfolio">
+          <AiOutlineArrowDown />
+          </a>
+      </div>
+    </div>
   )
 }
