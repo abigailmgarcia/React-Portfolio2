d100bd2cfc3f1c51957ea6bd970ff31181aff708
abigailmgarcia <abigailmg31@gmail.com>
2023-08-01 16:23:53 -0500

    styled icons

--- abigails-portfolio/src/components/navbar/Navbar.jsx
+++ abigails-portfolio/src/components/navbar/Navbar.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import "./navbar.scss"
+import { BsFillPersonFill } from "react-icons/bs"
+import { IoMdMail } from "react-icons/io";
 
 export default function Navbar() {
   return (
@@ -8,7 +10,12 @@ export default function Navbar() {
           <div className="left">
             <a href="#intro" className='logo'>hello</a>
             <div className="itemcontainer">
-              
+              <BsFillPersonFill className='icon'/>
+              <span> +44 </span>
+            </div>
+            <div className="itemcontainer">
+              < IoMdMail className='icon' />
+              <span> abigail@gmail.com </span>
             </div>
           </div>
           <div className="right">
