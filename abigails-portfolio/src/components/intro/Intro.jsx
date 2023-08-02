8e85a4b21167ee0cc8ab2d2cedab4edc626f27fd
abigailmgarcia <abigailmg31@gmail.com>
2023-08-01 21:52:11 -0500

    made intro more resonsive

--- abigails-portfolio/src/components/intro/Intro.jsx
+++ abigails-portfolio/src/components/intro/Intro.jsx
@@ -18,7 +18,7 @@ export default function intro() {
         <div className="wrapper">
           <h2>Hello Everyone, I am</h2>
           <h1>Abigail Garcia</h1>
-          <h3> A <span> Full stack web deb</span></h3>
+          <h3><span> FullStack Web Dev</span></h3>
         </div>
         <a href="#portfolio">
           <AiOutlineArrowDown />
