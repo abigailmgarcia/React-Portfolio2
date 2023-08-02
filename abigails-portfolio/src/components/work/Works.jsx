5fe04f45975ee5c29030e3b0e15f7475a880ceef
abigailmgarcia <abigailmg31@gmail.com>
2023-08-01 19:44:09 -0500

    added hover feature over projects

--- abigails-portfolio/src/components/work/Works.jsx
+++ abigails-portfolio/src/components/work/Works.jsx
@@ -4,7 +4,35 @@ import './works.scss'
 export default function Works() {
   return (
     <div className='works' id="works">
-      
+      <h1>Portfolio</h1>
+      <ul>
+        <li className='active'>Short Stack</li>
+        <li>Web</li>
+        <li>Mobile App</li>
+        <li>design</li>
+      </ul>
+      <div className="container">
+        <div className='item'>
+        <img src="./assets/shortstack.png" alt="" />
+        <h3>ShortStack</h3>
+      </div>
+      <div className="item">
+        <img src="./assets/shortstack.png" alt="" />
+        <h3>ShortStack</h3>
+      </div>
+      <div className="item">
+        <img src="./assets/shortstack.png" alt="" />
+        <h3>ShortStack</h3>
+      </div>
+      <div className="item">
+        <img src="./assets/shortstack.png" alt="" />
+        <h3>ShortStack</h3>
+      </div>
+      <div className="item">
+        <img src="./assets/shortstack.png" alt="" />
+        <h3>ShortStack</h3>
+      </div>
+      </div>
     </div>
   )
 }
