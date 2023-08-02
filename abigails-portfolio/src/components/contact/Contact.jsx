d93d36510b1632be43b695d95e98c84e31f9d9af
abigailmgarcia <abigailmg31@gmail.com>
2023-08-01 21:24:55 -0500

    added form for contact

--- abigails-portfolio/src/components/contact/Contact.jsx
+++ abigails-portfolio/src/components/contact/Contact.jsx
@@ -2,7 +2,23 @@ import React from 'react'
 import "./contact.scss"
 
 export default function Contact() {
+
+  const handleSumbit = (e) => {
+    e.preventDefault();
+  }
   return (
-    <div className='contact' id="contact"></div>
+    <div className='contact' id="contact">
+      <div className="left">
+        <img src="assets/message.jpg" alt="" />
+      </div>
+      <div className="right">
+        <h2>Contact</h2>
+        <form onSubmit={handleSumbit}>
+          <input type="text" placeholder="Email"/>
+          <textarea placeholder="Message"></textarea>
+          <button type='submit'>Send</button>
+        </form>
+      </div>
+    </div>
   )
 }
